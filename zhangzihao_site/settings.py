751f5105ae63d4c25d7e239856b9f101cbe4a554
zh-zih <zhang.zih@outlook.com>
2025-12-24 16:56:10 -0600

    fix staticroot

--- zhangzihao_site/settings.py
+++ zhangzihao_site/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
